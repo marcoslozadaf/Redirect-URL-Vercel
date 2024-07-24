0fec8d49dac7e71e6970063a6e171079d8835450
Marcos Lozada Freytes <marcoslozadaf@hotmail.com>
2024-07-24 17:09:01 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  
+  const router = useRouter()
+  router.replace("https://www.marcoslozada.com/") //your URL eg: https://www.marcoslozada.com, https://www.linkedin.com/in/marcoslozadafreytes/, https://x.com/marcoslozadaf, etc.
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+    </main>
+  );
+}
